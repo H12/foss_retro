a69ff469f53016beb5477b3c61ba12d3a617854f
Henry Firth <henrynf12@gmail.com>
2020-06-09 20:59:29 -0500

    Add super basic board creation view

---
+++ lib/easy_retro_web/live/create_live.ex
@@ -0,0 +1,15 @@
+defmodule EasyRetroWeb.CreateLive do
+  use EasyRetroWeb, :live_view
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, key: nil, title: "")}
+  end
+
+  @impl true
+  def handle_event("create", %{"title" => title}, socket) do
+    key = EasyRetro.build_board(title)
+
+    {:noreply, assign(socket, key: key, title: title)}
+  end
+end
--- lib/easy_retro_web/router.ex
+++ lib/easy_retro_web/router.ex
@@ -17,7 +17,7 @@ defmodule EasyRetroWeb.Router do
   scope "/", EasyRetroWeb do
     pipe_through :browser
 
-    live "/", PageLive, :index
+    live "/", CreateLive, :index
   end
 
   # Other scopes may use custom stacks.
