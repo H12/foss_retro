fb750c9f3306cebfc9764fe7ae3a5e9d977cbc51
Henry Firth <henrynf12@gmail.com>
2020-06-13 12:56:31 -0500

    Add retro board lookup via URL params

--- lib/easy_retro_web/live/board_live.ex
+++ lib/easy_retro_web/live/board_live.ex
@@ -1,6 +1,10 @@
 defmodule EasyRetroWeb.BoardLive do
   use EasyRetroWeb, :live_view
 
+  def mount(%{"key" => key}, _session, socket) do
+    {:ok, assign(socket, board: EasyRetro.lookup_board_by_key(key), key: key)}
+  end
+
   def mount(_params, _session, socket) do
     {:ok, assign(socket, board: nil, key: nil)}
   end
