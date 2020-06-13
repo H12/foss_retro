d9e85dd59bef32b78566e4d624194aa154c544e8
Henry Firth <henrynf12@gmail.com>
2020-06-13 09:35:12 -0500

    Clean up some cruft

--- lib/easy_retro_web/live/list_live.ex
+++ lib/easy_retro_web/live/list_live.ex
@@ -4,17 +4,12 @@ defmodule EasyRetroWeb.ListLive do
   @impl true
   def mount(_params, _session, socket) do
     EasyRetro.subscribe()
-    {:ok, assign(socket, title: "", boards: EasyRetro.list_boards())}
+    {:ok, assign(socket, boards: EasyRetro.list_boards())}
   end
 
   @impl true
   def handle_info({EasyRetro, [:board, :built], _code}, socket) do
     {:noreply, fetch_boards(socket)}
-  end
-
-  def handle_info(debug) do
-    IO.puts("DEBUG INFO:")
-    IO.inspect(debug)
   end
 
   defp fetch_boards(socket) do
