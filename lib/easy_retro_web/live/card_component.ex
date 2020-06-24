5a4953955fdf3f1d1371104ecf28ac612f23ec29
Henry Firth <henrynf12@gmail.com>
2020-06-24 08:31:50 -0500

    Add some dummy icons

--- lib/easy_retro_web/live/card_component.ex
+++ lib/easy_retro_web/live/card_component.ex
@@ -3,7 +3,17 @@ defmodule CardComponent do
 
   def render(assigns) do
     ~L"""
-    <div class="card"><span class="content"><%= @content %></span></div>
+    <div class="card">
+      <div class="icons icons-top">
+        <i class="lni lni-sm lni-close"></i>
+      </div>
+      <div class="content">
+        <span><%= @content %></span>
+      </div>
+      <div class="icons icons-bottom">
+        <i class="lni lni-sm lni-bubble"></i>
+      </div>
+    </div>
     """
   end
 end
