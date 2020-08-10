5aa755bfc49b05364e2154eeb2c7e32a7715a3ca
Henry Firth <henrynf12@gmail.com>
2020-08-09 19:58:06 -0500

    Add functionality to EasyRetro for adding votes

--- lib/easy_retro.ex
+++ lib/easy_retro.ex
@@ -1,6 +1,6 @@
 defmodule EasyRetro do
   @moduledoc """
-  The main API for interacting with EasyRetro's OTP business logic
+  The main API for interacting with EasyRetro's OTP business logic.
   """
   alias EasyRetro.Boundary.{BoardManager, BoardSession}
   alias EasyRetro.Core.Board
@@ -156,6 +156,46 @@ defmodule EasyRetro do
     board
     |> registry_name_for_board()
     |> BoardSession.add_category(category_name)
+    |> BoardManager.update_board()
+    |> notify_subscribers([:board, :updated])
+  end
+
+  @doc """
+  Increments the number of votes on a given card, and records that vote for the
+  relevant voter. If a voter with the provided identifier does not exist, a new
+  voter will be added using that identifier.
+
+  ## Parameters
+    - board: Board struct containing the target Card
+    - voter_id: A String that identifies a specific voter
+    - card_id: A non-negative integer corresponding to a particular card
+
+  ## Examples
+
+      iex> cat_board = "Animals"
+      ...> |> EasyRetro.build_board()
+      ...> |> EasyRetro.add_category("Cats")
+      ...> |> EasyRetro.add_card(0, "Maru")
+      ...> |> EasyRetro.add_card(0, "Garfield")
+      iex> cat_board.cards[0]
+      %Card{id: 0, content: "Maru", votes: 0}
+      iex> cat_board.cards[1]
+      %Card{id: 1, content: "Garfield", votes: 0}
+      iex> cat_board.voters
+      %{}
+      iex> cat_board = EasyRetro.add_vote(cat_board, "ANewUser", 0)
+      iex> cat_board.cards[0]
+      %Card{id: 0, content: "Maru", votes: 1}
+      iex> cat_board.cards[1]
+      %Card{id: 1, content: "Garfield", votes: 0}
+      iex> cat_board.voters
+      %{"ANewUser" => [0]}
+  """
+  @spec add_vote(Board.t(), String.t(), non_neg_integer()) :: Board.t()
+  def add_vote(board, voter_id, card_id) do
+    board
+    |> registry_name_for_board()
+    |> BoardSession.add_vote(voter_id, card_id)
     |> BoardManager.update_board()
     |> notify_subscribers([:board, :updated])
   end
--- lib/easy_retro/boundary/board_session.ex
+++ lib/easy_retro/boundary/board_session.ex
@@ -56,6 +56,12 @@ defmodule EasyRetro.Boundary.BoardSession do
   end
 
   @impl GenServer
+  def handle_call({:add_vote, voter_id, card_id}, _from, board) do
+    new_board = Board.add_vote(board, voter_id, card_id)
+    {:reply, new_board, new_board}
+  end
+
+  @impl GenServer
   def handle_call({:view_board}, _from, board) do
     {:reply, board, board}
   end
@@ -70,6 +76,10 @@ defmodule EasyRetro.Boundary.BoardSession do
 
   def add_category(registry_name, name) do
     GenServer.call(via(registry_name), {:add_category, name})
+  end
+
+  def add_vote(registry_name, voter_id, card_id) do
+    GenServer.call(via(registry_name), {:add_vote, voter_id, card_id})
   end
 
   def view_board(registry_name) do
