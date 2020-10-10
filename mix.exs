7da643b0f463a81f32a2ff5dc3f2bc6e2bc14d4b
Henry Firth <henrynf12@gmail.com>
2020-10-09 21:00:45 -0500

    Upgrade phoenix and live_view

--- mix.exs
+++ mix.exs
@@ -33,8 +33,8 @@ defmodule EasyRetro.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.4"},
-      {:phoenix_live_view, "~> 0.13.0"},
+      {:phoenix, "~> 1.5.5"},
+      {:phoenix_live_view, "~> 0.14.7"},
       {:floki, ">= 0.0.0", only: :test},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
