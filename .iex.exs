dfd4a5909cb05fe7f307ae0dc4863c4672126ad4
Henry Firth <henrynf12@gmail.com>
2021-02-20 10:06:35 -0600

    Add some setup helpers to .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -14,3 +14,31 @@ defmodule Helpers do
     :ok
   end
 end
+
+# Module Aliases
+alias EasyRetro.Core.Board
+alias EasyRetro.Core.Card
+
+# Helpful variable setup
+sample_board =
+  Board.new("Sample Board")
+  |> Board.add_category("Cats")
+  |> Board.add_category("Dergs")
+  |> Board.add_card(0, "Maru")
+  |> Board.add_card(0, "Lil Bub")
+  |> Board.add_card(0, "Grumpy")
+  |> Board.add_card(1, "Boo")
+  |> Board.add_card(1, "Big Red")
+  |> Board.add_card(1, "Marnie")
+  |> Board.add_voter(0)
+  |> Board.add_vote(0, 0)
+  |> Board.add_vote(0, 1)
+  |> Board.add_vote(0, 5)
+  |> Board.add_voter(1)
+  |> Board.add_vote(1, 1)
+  |> Board.add_vote(1, 2)
+  |> Board.add_vote(1, 3)
+  |> Board.add_voter(2)
+  |> Board.add_vote(2, 0)
+  |> Board.add_vote(2, 1)
+  |> Board.add_vote(2, 2)
